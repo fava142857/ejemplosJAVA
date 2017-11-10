a24af51beec3c5147a283a2f4b022c79a336cd77
favaOtac <fvaldez@otac.mx>
2017-11-10 12:14:29 -0600

    Version inicial del proyecto

---
+++ src/com/mx/fava/Fecha/ManejoFecha.java
@@ -0,0 +1,17 @@
+package com.mx.fava.Fecha;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class ManejoFecha {
+
+	public static void main(String[] args) {
+		  LocalDate date = LocalDate.now();
+		  DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy MM dd");
+		  String text = date.format(formatter);
+		  LocalDate parsedDate = LocalDate.parse(text, formatter);
+		  System.out.println(parsedDate);
+		  System.out.println(text);
+	}
+	
+}
